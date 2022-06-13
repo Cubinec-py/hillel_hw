ac01fba5b0c2f47168533ef62a6765e35770345d
Cubinec-py <Ujyxfhtyrj95>
2022-06-13 14:04:06 +0300

    hw_fix

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 def parse(query: str) -> dict:
-    reformat = query.replace('/', '?').replace(':', '?').replace(';', '?').replace('&', '?').split('?')
-    parsed_dict = {second[0]: second[1] for second in [first.split('=') for first in reformat if '=' in first]
-                   if len(second[0]) > 1 and len(second[1]) > 1}
+    if '?' not in query:
+        return {}
+    else:
+        no_need, needed = query.split('?')
+        refactor = [i.split('=', 1) for i in needed.split('&')]
+        parsed_dict = {val[0]: val[1] for val in refactor if len(val[0]) and len(val[1]) > 1}
     return parsed_dict
 
 
@@ -11,25 +14,21 @@ if __name__ == '__main__':
     assert parse('http://example.com/') == {}
     assert parse('http://example.com/?') == {}
     assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
-    assert parse('https://example.com/path/to/page:name=ferret:color=purple') == {'name': 'ferret', 'color': 'purple'}
-    assert parse('https://example.com/path/to/page?name=ferret&color=?') == {'name': 'ferret'}
-    assert parse('http://example.com/?&name=Dima=') == {'name': 'Dima'}
-    assert parse('http://example.com/?&=Dima/name=Dima') == {'name': 'Dima'}
+    assert parse('https://example.com/path/to/page:name=ferret:color=purple') == {}
+    assert parse('https://example.com/path/to/page?name=ferret&color=') == {'name': 'ferret'}
+    assert parse('http://example.com/?&name=Dima=') == {'name': 'Dima='}
+    assert parse('http://example.com/?&Dima/name=Dima') == {'Dima/name': 'Dima'}
     assert parse('https://example.com/path/to/page:ferret/color:purple&name') == {}
-    assert parse('https://example.com/path/to/?:color/=purple&&color=purple') == {'color': 'purple'}
+    assert parse('https://example.com/path/to/?:color/=purple&&color=purple') == {':color/': 'purple', 'color': 'purple'}
     assert parse('http://example.com/?&=:;') == {}
-    assert parse('http://example.com/?=Dima?=name/color=&=purple/') == {}
-    assert parse('https://example.com/path/to/?:color/=purple==color/purlple') == {}
-    assert parse('https://example.com/path/to/page?name=ferret=color=?') == {'name': 'ferret'}
+    assert parse('http://example.com/?Dima=name/color=&=purple/') == {'Dima': 'name/color='}
+    assert parse('https://example.com/path/to/?:color/=purple==color/purlple') == {':color/': 'purple==color/purlple'}
+    assert parse('https://example.com/path/to/page?name=ferret=color=') == {'name': 'ferret=color='}
 
 
 def parse_cookie(query: str) -> dict:
-    reformat = query.replace('/', '?').replace(':', '?').replace(';', '?').replace('&', '?').split('?')
-    parsed_cookie = {
-        second[0]: second[1] for second in [first.split('=', 1) for first in reformat if '=' in first]
-        if (len(second[0]) > 1 and '=' not in second[:][0][0])
-           and (len(second[1]) > 1 and '=' not in second[:][1][0])
-    }
+    refactor = [i.split('=', 1) for i in query.split(';')]
+    parsed_cookie = {val[0]: val[1] for val in refactor if len(val) > 1}
     return parsed_cookie
 
 
@@ -38,13 +37,13 @@ if __name__ == '__main__':
     assert parse_cookie('') == {}
     assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
     assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
-    assert parse_cookie('name=Dima//age==28;') == {'name': 'Dima'}
-    assert parse_cookie('name==Dima//age==28;') == {}
-    assert parse_cookie('name=/Dima;age=28;') == {'age': '28'}
-    assert parse_cookie('name===Dima/User;age=28;') == {'age': '28'}
+    assert parse_cookie('name=Dima//age==28;') == {'name': 'Dima//age==28'}
+    assert parse_cookie('name==Dima//age==28;') == {'name': '=Dima//age==28'}
+    assert parse_cookie('name=/Dima;age=28;') == {'name': '/Dima', 'age': '28'}
+    assert parse_cookie('name===Dima/User;age=28;') == {'name': '==Dima/User', 'age': '28'}
     assert parse_cookie('name/Dima:User;age?28;') == {}
     assert parse_cookie('name=Dima=User;age=28=Dima;') == {'name': 'Dima=User', 'age': '28=Dima'}
     assert parse_cookie('name=Dima=User=Dima;age//28;') == {'name': 'Dima=User=Dima'}
-    assert parse_cookie('name/Dima=User=Dima;age?28;') == {'Dima': 'User=Dima'}
-    assert parse_cookie('name/Dima=User=Dima=age=28;') == {'Dima': 'User=Dima=age=28'}
-    assert parse_cookie('name/Dima=User&/Dima?age===28;') == {'Dima': 'User'}
+    assert parse_cookie('name/Dima=User=Dima;age?28;') == {'name/Dima': 'User=Dima'}
+    assert parse_cookie('name/Dima=User=Dima=age=28;') == {'name/Dima': 'User=Dima=age=28'}
+    assert parse_cookie('name/Dima=User&/Dima?age===28;') == {'name/Dima': 'User&/Dima?age===28'}
